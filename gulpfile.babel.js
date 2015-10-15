fb25714f62ed6a6f5ab235da0f4c8b6c608355e7
Andrey Sitnik <andrey@sitnik.ru>
2015-10-15 19:26:10 +0100

    Fix del API

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,8 @@
 import gulp from 'gulp';
 
-gulp.task('clean', (done) => {
+gulp.task('clean', () => {
     let del = require('del');
-    del(['build/'], done);
+    return del(['build/']);
 });
 
 // Build
