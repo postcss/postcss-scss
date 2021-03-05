812b83c74cd057756bd54396c83e7bae0dfb5c3e
Andrey Sitnik <andrey@sitnik.ru>
2021-03-04 22:08:37 -0500

    Move from Travis CI to GitHub Actions

--- test/integration.js
+++ test/integration.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
 
 let { testOnReal } = require('postcss-parser-tests')
-let ciJobNumber = require('ci-job-number')
 let postcss = require('postcss')
 
 let scss = require('../')
 
-if (ciJobNumber() === 1) {
-  testOnReal(css =>
-    postcss().process(css, {
-      parser: scss,
-      map: { annotation: false }
-    })
-  )
-}
+testOnReal(css =>
+  postcss().process(css, {
+    parser: scss,
+    map: { annotation: false }
+  })
+)
